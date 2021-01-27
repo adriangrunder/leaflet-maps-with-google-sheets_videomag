935d744a5f32e29af8ff531de206780e4e1ca9ec
adriangrunder <74954532+adriangrunder@users.noreply.github.com>
2021-01-27 17:52:49 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -129,7 +129,7 @@ $(window).on('load', function() {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
-          point['Beschreibung']);
+          point['Description']);
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
@@ -820,8 +820,8 @@ $(window).on('load', function() {
             pane: 'shadowPane'
           }).addTo(map);
 
-          if (p[index]['Beschreibung'] && p[index]['Beschreibung'] != '') {
-            line.bindPopup(p[index]['Beschreibung']);
+          if (p[index]['Description'] && p[index]['Description'] != '') {
+            line.bindPopup(p[index]['Description']);
           }
 
           polylinesLegend.addOverlay(line,
