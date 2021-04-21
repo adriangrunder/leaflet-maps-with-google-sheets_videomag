c57c28f97d2003b5830966d5808118d0a599d226
adriangrunder <74954532+adriangrunder@users.noreply.github.com>
2021-04-21 13:08:41 +0200

    Revert "Update google-doc-url.js"
    This reverts commit 9ba4370160f27a0411c3965e231d120d88e1e628.

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/15KVkKA0DT2HADelgi-M9DcCUROj3JcEqQE8EHRNJ6gA/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBGpQvD3cSUyPIwt-FY2-rbvP0pxs_ITeo';
+var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
